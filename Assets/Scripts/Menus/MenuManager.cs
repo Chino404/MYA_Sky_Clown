4896906449e9e18994562ba80562d69d8abdc6ba
Matias Sepulveda <ezesepulveda444@gmail.com>
2023-12-27 23:05:25 -0300

    Proyecto Final

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -5,21 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
-   public void MainMenu()
+   
+
+    private void Start()
+    {
+        
+        //lastScene = SceneManager.GetActiveScene().buildIndex;
+
+       
+    }
+    public void MainMenu()
     {
         SceneManager.LoadScene(1);
     }
     public void PlayButton()
     {
-        SceneManager.LoadScene(2);
+        if (PauseManager.lastScene ==0)
+            SceneManager.LoadScene(2);
+        else
+            SceneManager.LoadScene(PauseManager.lastScene);
         Time.timeScale = 1;
     }
     public void TutorialLevel()
     {
         SceneManager.LoadScene(3);
         Time.timeScale = 1;
-
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
